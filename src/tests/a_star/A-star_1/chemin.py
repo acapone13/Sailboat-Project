56817808cac87f607c9544957f9f4aba2f5b7e61
acapone13 <caponeaugusto@gmail.com>
2019-05-07 14:54:39 +0200

    Integration A* algorithms

--- src/tests/a_star/A-star_1/chemin.py
+++ src/tests/a_star/A-star_1/chemin.py
@@ -6,8 +6,8 @@ class Node():
         self.parent = parent
         self.position = position
 
-        self.g = 0     # g est la relation entre la position et la position finale
-        self.h = 0	   # h est la relation entre la position et la position initiale
+        self.g = 0     # g est la relation entre la position et la position initiale
+        self.h = 0	   # h est la relation la moins cher entre la position et la position finale
         self.f = 0	   # f est la somme entre g et h
 
     def __eq__(self, other):
@@ -16,8 +16,8 @@ class Node():
 def astar(maze, start, end):
 
     # Cree les nodes start et end
-    node_inicial = Node(None, start)
-    node_inicial.g = node_inicial.h = node_inicial.f = 0
+    node_initial = Node(None, start)
+    node_initial.g = node_initial.h = node_initial.f = 0
     node_final = Node(None, end)
     node_final.g = node_final.h = node_final.f = 0
 
@@ -26,7 +26,7 @@ def astar(maze, start, end):
     closed_list = []
 
     # Ajoute le node start
-    open_list.append(node_inicial)
+    open_list.append(node_initial)
 
     # Loop jusqu'a trouver le bout
     while len(open_list) > 0:
@@ -82,8 +82,8 @@ def astar(maze, start, end):
                     continue
 
             #Cree les f, g, and h valeurs
-            child.g = current_node.g + 1
-            child.h = ((child.position[0] - node_final.position[0]) ** 2) + ((child.position[1] - node_final.position[1]) ** 2)
+            child.g = cost_of_path(current_node)
+            child.h = heuristic(child, node_final, current_node)
             child.f = child.g + child.h
 
             # Child est déjà dans l'open list
@@ -93,6 +93,20 @@ def astar(maze, start, end):
 
             # Ajoute le child dans l'open list
             open_list.append(child)
+
+def cost_of_path(current_node):
+    # cost of the path from the start node to n
+    return current_node.g + 1
+
+def heuristic(node_final, child, current_node):
+    #  cost of the cheapest path from n to the goal
+    if (child.position[0] > current_node.position[0] or child.position[0] < current_node.position[0]) and child.position[1] > current_node.position[1]:  
+        h = ((child.position[0] - node_final.position[0]) ** 2) + ((child.position[1] - node_final.position[1]) ** 2)
+    elif child.position[1] == current_node.position[1]:
+        h = 10
+    else:
+        h = 10
+    return h
 
 
 def main():
