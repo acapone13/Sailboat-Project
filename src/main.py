b577f815d8a4ed8e8448acd7929eee852932e138
Pedro Fontana <pedro.fontana.1996@gmail.com>
2019-05-29 12:20:23 +0200

    Adds docs/Instructions to presentation; Interface dummy test; modifies parser to one mandatory parameter (astar, rl, ..)

--- src/main.py
+++ src/main.py
@@ -1,42 +1,58 @@
 import helpers, argparse, gym, gym_voilier
 from astar import Astar
 from rl import Rl
+from dummy import Dummy
 
 # Arguments parser
 parser = argparse.ArgumentParser(prog = "Sailboat-Project")
 parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
-parser.add_argument("-a", "--astar", action="store_true", help = "Run astar module")
-parser.add_argument("-r", "--rl", action="store_true", help = "Run reinforcement learning module")
+# parser.add_argument("-a", "--astar", action="store_true", help = "Run astar module")
+# parser.add_argument("-r", "--rl", action="store_true", help = "Run reinforcement learning module")
+# parser.add_argument("-d", "--dummy", action="store_true", help = "Dummy Test")
+parser.add_argument("model", choices=["astar", "rl", "dummy"], help = "Model to use" )
 args = parser.parse_args()
 
-def valid_args(args):
-    """
-    Validate if the arguments are valid
-    """
-    if args.astar and args.rl:
-        print("Please choose only one module, -h for more information.")
-        exit()
-    elif not args.astar and not args.rl:
-        print("Please choose one module, -h for more information.")
-        exit()
+# def valid_args(args):
+#     """
+#     Validate if the arguments are valid
+#     """
+#     if args.astar and args.rl:
+#         print("Please choose only one module, -h for more information.")
+#         exit()
+#     elif not args.astar and not args.rl and not args.dummy:
+#         print("Please choose one module, -h for more information.")
+#         exit()
 
 def main(args):
     
-    # Astar module
-    if args.astar:
+    # # Astar module
+    # if args.astar:
+    #     model = Astar()
+    # # Reinforcement learning module
+    # elif args.rl:
+    #     model = Rl()
+    # elif args.dummy:
+    #     model = Dummy()
+
+    # Astar Module 
+    if args.model == "astar":
         model = Astar()
-    # Reinforcement learning module
-    elif args.rl:
+    # Reinforcement Learning module
+    elif args.model == "rl":
         model = Rl()
+    # Dummy test 
+    elif args.model == "dummy":
+        model = Dummy()
 
     # Create an environment
     env = gym.make('voilier-v2').unwrapped
     state = env.reset()
     # TODO Get range by from argument
-    for step in range(0,20,1):
-        state = model.step(state)
+    for step in range(0,200,1):
+        action = model.step(state)
+        state, _, _, _ = env.step(action)
         env.render()
 
 if __name__ == "__main__":
-    valid_args(args)
+    # valid_args(args)
     main(args)
---
+++ src/modules/dummy.py
@@ -0,0 +1,17 @@
+from AI import IAI
+import numpy as np
+import random
+
+class Dummy(IAI):
+    """
+    Module used to implement a random behaviour astar from AI interface
+    """
+
+    def step(self, params):
+        # Params not used
+        action = np.zeros(9)
+        action[random.randrange(9)]=1
+        return action.reshape(1,9)    
+        
+    def log(self, message):
+        print(message)
