4fc9e44990e03acb578f7c4fc665e4a2e96a0941
Pedro Fontana <pedro.fontana.1996@gmail.com>
2019-05-29 12:49:39 +0200

    Removes comments

--- src/main.py
+++ src/main.py
@@ -6,34 +6,12 @@ from dummy import Dummy
 # Arguments parser
 parser = argparse.ArgumentParser(prog = "Sailboat-Project")
 parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
-# parser.add_argument("-a", "--astar", action="store_true", help = "Run astar module")
-# parser.add_argument("-r", "--rl", action="store_true", help = "Run reinforcement learning module")
-# parser.add_argument("-d", "--dummy", action="store_true", help = "Dummy Test")
 parser.add_argument("model", choices=["astar", "rl", "dummy"], help = "Model to use" )
 args = parser.parse_args()
 
-# def valid_args(args):
-#     """
-#     Validate if the arguments are valid
-#     """
-#     if args.astar and args.rl:
-#         print("Please choose only one module, -h for more information.")
-#         exit()
-#     elif not args.astar and not args.rl and not args.dummy:
-#         print("Please choose one module, -h for more information.")
-#         exit()
 
 def main(args):
     
-    # # Astar module
-    # if args.astar:
-    #     model = Astar()
-    # # Reinforcement learning module
-    # elif args.rl:
-    #     model = Rl()
-    # elif args.dummy:
-    #     model = Dummy()
-
     # Astar Module 
     if args.model == "astar":
         model = Astar()
@@ -54,5 +32,4 @@ def main(args):
         env.render()
 
 if __name__ == "__main__":
-    # valid_args(args)
     main(args)
